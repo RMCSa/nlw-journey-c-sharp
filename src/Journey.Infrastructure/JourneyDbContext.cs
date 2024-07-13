0c87ff7427689d693b292e1668ecb64cbe08c507
Rafael Moreira Cavalcante de Souza <125597354+RMCSa@users.noreply.github.com>
2024-07-12 22:39:29 -0300

    Adicionar arquivos de projeto.

---
+++ src/Journey.Infrastructure/JourneyDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Journey.Infrastructure.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Journey.Infrastructure
+{
+    public class JourneyDbContext : DbContext
+    {
+        public DbSet<Trip> Trips { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=C:\\Users\\Rafael\\Downloads\\JourneyDatabase.db");
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Activity>().ToTable("Activities");
+        }
+
+    }
+}
